2a6c19a1326d07d64563a3a5095a4e95fdaa7cc1
Andrii-Rodchenko <rod4enkoandriy@gmail.com>
2022-05-16 17:36:49 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const { elements: { email, password }
+    } = event.currentTarget;
+    
+
+    if (email.value === "" || password.value === "") {
+        return alert("Все поля должны быть заполнены");
+    }
+    
+    const formData = new FormData(event.currentTarget)
+
+    let formОbject = {
+    }
+     formData.forEach((value, name) => {
+         return formОbject[name] = value;
+    } )
+    console.log(formОbject)
+
+     event.currentTarget.reset();
+
+}
+
+
+
+
